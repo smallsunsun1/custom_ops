add Keras-like wrapper for DeformableConv2DLayer and fix problem in backprop op

deformable_conv2d_ops_new/deformable_conv2d.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import os.path as osp
 from tensorflow.python.framework import ops
+import tensorflow.keras as keras
 import math
 
 filename = osp.join(osp.dirname(__file__), 'deformable_conv2d.so')
@@ -63,8 +64,70 @@ def _deformable_conv2d_back_prop(op, grad):
         .Attr("dilations: list(int) = [1, 1, 1, 1]")
     '''
     # compute gradient
-    data_grad = deformable_conv2d_grad_op(data, filter, offset, mask, grad, strides, num_groups, deformable_groups, im2col_step, no_bias, pads, data_format, dilations)
+    data_grad = deformable_conv2d_grad_op(data, filter,
+                                          offset, mask,
+                                          grad, strides=strides,
+                                          num_groups=num_groups,
+                                          deformable_groups=deformable_groups,
+                                          im2col_step=im2col_step,
+                                          no_bias=no_bias,
+                                          padding=pads,
+                                          data_format=data_format,
+                                          dilations=dilations)
     return data_grad # List of 4 Tensor, since we have 4 input
+
+
+class DeformableConv2D(keras.layers.Layer):
+    def __init__(self, filters, kernel_size=(3, 3), num_groups=1, deformable_groups=1, strides=(1, 1, 1, 1), im2col=1,
+                 use_bias=False, padding="VALID", data_format='NCHW', dilations=(1, 1, 1, 1)):
+        super(DeformableConv2D, self).__init__()
+        self.filters = filters
+        self.kernel_size = kernel_size
+        self.num_groups = num_groups
+        self.deformable_groups = deformable_groups
+        self.strides = strides
+        self.im2col = im2col
+        self.use_bias = use_bias
+        self.padding = padding
+        self.data_format = data_format
+        self.dilations = dilations
+        self.conv_offset = keras.layers.Conv2D(self.deformable_groups * 3 * self.kernel_size[0] * self.kernel_size[1],
+                                               kernel_size=self.kernel_size, strides=(1, 1), padding="same", use_bias=True,
+                                               data_format="channels_last")
+
+    def build(self, input_shape):
+        channel = int(input_shape[-1])
+        self.filter = tf.Variable(initial_value=tf.random.normal(shape=[self.filters, channel, self.kernel_size[0], self.kernel_size[1]]))
+        self.built = True
+
+    def call(self, inputs, **kwargs):
+        """
+        Build static Graph
+        :param inputs: [B, Height, Width, Channel]
+        :param kwargs:
+        :return:
+        """
+        inputs_data = tf.transpose(inputs, [0, 3, 1, 2])
+        weight_info = self.conv_offset(inputs)
+        weight_info = tf.transpose(weight_info, [0, 3, 1, 2])
+        o1, o2, mask = tf.split(weight_info, 3, axis=1)
+        offset = tf.concat((o1, o2), axis=1)
+        mask = tf.sigmoid(mask)
+        result = deformable_conv2d_module.deformable_conv2d(
+            input=inputs_data,
+            filter=self.filter,
+            offset=offset,
+            mask=mask,
+            strides=self.strides,
+            num_groups=self.num_groups,
+            deformable_groups=self.deformable_groups,
+            im2col_step=self.im2col,
+            no_bias=(not self.use_bias),
+            padding=self.padding,
+            data_format=self.data_format,
+            dilations=self.dilations)
+        result = tf.transpose(result, [0, 2, 3, 1])
+        return result
 
 
 if __name__ == '__main__':
@@ -72,41 +135,53 @@ if __name__ == '__main__':
     width = 5
     kernel_h = 3
     kernel_w = 3
+    channel = 3
+    out_channel = 1
     padding = "SAME"
-    # input = tf.random.uniform(shape=[1, 3, height, width], maxval=10)
-    input = tf.ones(shape=[1, 1, height, width])
-    input_b = tf.pad(input, [[0, 0], [0, 0], [1, 1], [1, 1]])
-    filter = tf.Variable(tf.ones(shape=[kernel_h, kernel_w, 1, 1]))
-    filter_deform = tf.transpose(filter, [3, 2, 0, 1])
+    with tf.GradientTape(persistent=True) as tape:
+        # input = tf.random.uniform(shape=[1, 3, height, width], maxval=10)
+        input = tf.random.uniform(shape=[1, channel, height, width], maxval=10)
+        input_b = tf.pad(input, [[0, 0], [0, 0], [1, 1], [1, 1]])
+        input_trans = tf.transpose(input, [0, 2, 3, 1])
+        filter = tf.Variable(tf.ones(shape=[kernel_h, kernel_w, channel, out_channel]))
+        filter_deform = tf.transpose(filter, [3, 2, 0, 1])
+        tape.watch(filter)
+        offset = tf.constant([0. for i in range(kernel_h * kernel_w * 2 * height * width)], shape=[1, kernel_h * kernel_w * 2, height, width])
+        mask = tf.constant([1. for i in range(kernel_h * kernel_w * height* width)], shape=[1, kernel_h * kernel_w, height, width])
+        result = deformable_conv2d_module.deformable_conv2d(
+            input=input,
+            filter=filter_deform,
+            offset=offset,
+            mask=mask,
+            strides=[1, 1, 1, 1],
+            num_groups=1,
+            deformable_groups=1,
+            im2col_step=1,
+            no_bias=True,
+            padding=padding,
+            data_format='NCHW',
+            dilations=[1, 1, 1, 1])
+        result2 = deformable_conv2d_module.deformable_conv2d(
+            input=input_b,
+            filter=filter_deform,
+            offset=offset,
+            mask=mask,
+            strides=[1, 1, 1, 1],
+            num_groups=1,
+            deformable_groups=1,
+            im2col_step=1,
+            no_bias=True,
+            padding="VALID",
+            data_format='NCHW',
+            dilations=[1, 1, 1, 1])
+        deform_conv2d_layer = DeformableConv2D(out_channel, (kernel_w, kernel_h), padding=padding)
+        result3 = deform_conv2d_layer(input_trans)
+        result4 = tf.nn.conv2d(input_trans, filter, [1, 1, 1, 1], "SAME")
+        print(result)
+        print(result2)
+        print(tf.transpose(result3,[0, 3, 1, 2]))
+        grad1 = tape.gradient(result2, filter)
+        grad2 = tape.gradient(result4, filter)
+        print(grad1)
+        print(grad2)
 
-    offset = tf.constant([0. for i in range(kernel_h * kernel_w * 2 * height * width)], shape=[1, kernel_h * kernel_w * 2, height, width])
-    mask = tf.constant([1. for i in range(kernel_h * kernel_w * height* width)], shape=[1, kernel_h * kernel_w, height, width])
-    result = deformable_conv2d_module.deformable_conv2d(
-        input=input,
-        filter=filter_deform,
-        offset=offset,
-        mask=mask,
-        strides=[1, 1, 1, 1],
-        num_groups=1,
-        deformable_groups=1,
-        im2col_step=1,
-        no_bias=True,
-        padding=padding,
-        data_format='NCHW',
-        dilations=[1, 1, 1, 1])
-    result2 = deformable_conv2d_module.deformable_conv2d(
-        input=input_b,
-        filter=filter_deform,
-        offset=offset,
-        mask=mask,
-        strides=[1, 1, 1, 1],
-        num_groups=1,
-        deformable_groups=1,
-        im2col_step=1,
-        no_bias=True,
-        padding="VALID",
-        data_format='NCHW',
-        dilations=[1, 1, 1, 1])
-
-    print(result)
-    print(result2)